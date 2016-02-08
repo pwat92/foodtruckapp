394ef6319fee7de33a7910e785e4a2d0625c29ad
pwat92 <patrick.watson92@gmail.com>
2016-02-07 21:56:31 -0500

    added basic company managment functionality on truck side

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207224250) do
+ActiveRecord::Schema.define(version: 20160208014412) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "truck_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trucks", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
